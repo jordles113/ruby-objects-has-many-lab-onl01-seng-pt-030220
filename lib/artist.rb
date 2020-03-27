f447e7ae739ce7a45a63e055da97875e6d2f1324
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-27 21:43:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,15 @@
-class Artist 
-  
-  attr_accessor :name 
-  
-  @@all = []
-  
-  def intialize(name)
-    @name = name 
-    save 
-  end 
-  def add_song 
-    song.artist = self 
-  end 
-  
-  def save 
-    @@all << self 
-  end 
-end 
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def add_song(song)
+    song.artist = self
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+end
