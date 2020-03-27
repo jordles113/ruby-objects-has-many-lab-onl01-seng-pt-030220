414f0dec9c49f3ef14895bbc2c42227f8c9b5e6b
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-27 21:46:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,14 @@
 class Song 
+  attr_accessor :name, :artist 
+   
+  @@all = []
+  
+  def intialize(name, artist = nil)
+    @name = name
+    @artist = artist 
+    @@all << self 
+  end 
+  
+  def artist_name 
+  end 
 end 
